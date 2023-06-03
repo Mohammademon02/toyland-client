293c3702df13eaf1107badde39a918eef85eae62
mohammademon02 <mohammademon0207@gmail.com>
2023-06-03 06:07:12 +0600

    blog section added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Blog from "../pages/Blog/Blog";
@@ -10,54 +10,55 @@ import AddToy from "../pages/AddToy/AddToy";
 import ToyDetails from "../pages/Home/SingleToy/toyDetails";
 import MyToys from "../pages/MyToys/MyToys";
 import UpdateToy from "../pages/UpdateToy/UpdateToy";
+import PrivateRoutes from "./privateRoutes";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Main></Main> ,
+        element: <Main></Main>,
         errorElement: <ErrorPage></ErrorPage>,
         children: [
-           {
-            path: "/",
-            element: <Home></Home>
-           },
-           {
-            path: "addToy",
-            element: <AddToy></AddToy>
-           },
-           {
-            path: "allToys",
-            element: <AllToys></AllToys>
-           }
-           ,
-           {
-            path: "myToys",
-            element: <MyToys></MyToys>
-           }
-           ,
-           {
-            path: "login",
-            element: <Login></Login>
-           },
-           {
-            path: "register",
-            element: <Register></Register>
-           },
-           {
-            path: "blog",
-            element: <Blog></Blog>
-           },
-           {
-            path: "details/:id",
-            element: <ToyDetails></ToyDetails>,
-            loader:({params}) => fetch(`http://localhost:5000/toy/${params.id}`)
-           },
-           {
-            path: "myToys/:id",
-            element: <UpdateToy></UpdateToy>,
-               loader: ({ params }) => fetch(`http://localhost:5000/toy/${params.id}`)
-           }
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "addToy",
+                element: <PrivateRoutes><AddToy></AddToy></PrivateRoutes>
+            },
+            {
+                path: "allToys",
+                element: <AllToys></AllToys>
+            }
+            ,
+            {
+                path: "myToys",
+                element: <PrivateRoutes><MyToys></MyToys></PrivateRoutes>
+            }
+            ,
+            {
+                path: "login",
+                element: <Login></Login>
+            },
+            {
+                path: "register",
+                element: <Register></Register>
+            },
+            {
+                path: "blog",
+                element: <Blog></Blog>
+            },
+            {
+                path: "details/:id",
+                element: <PrivateRoutes><ToyDetails></ToyDetails></PrivateRoutes>,
+                loader: ({ params }) => fetch(`http://localhost:5000/toy/${params.id}`)
+            },
+            {
+                path: "myToys/:id",
+                element: <PrivateRoutes><UpdateToy></UpdateToy></PrivateRoutes>,
+                loader: ({ params }) => fetch(`http://localhost:5000/toy/${params.id}`)
+            }
         ]
     },
 ]);
